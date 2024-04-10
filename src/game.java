a22e0336e9bf8cc96452eafc4f5bf8ce69de4418
vr-ski <vr.ski@yahoo.com>
2024-04-10 23:42:45 +0300

    first draft: first commit

---
+++ src/game.java
@@ -0,0 +1,41 @@
+public class Game {
+	
+	private int size;
+	private int[size][size] grid;
+	
+	public Game(int newSize) {
+		size = newSize
+	}
+
+	private boolean registerMove(int x, int y, Player player) {
+		
+		boolean successful_outcome = false;
+
+		if grid[x][y] == "empty" {
+			grid[x][y] = player.getSymbol()
+			successful_outcome = true;
+		}
+
+		return successful_outcome;
+	
+	}
+
+	private boolean confirmWin(Player player) {
+		
+		// check if player wins
+		return
+	}
+
+	private boolean confirmEndOfGame() {
+		
+		// check end of game 
+		return
+	}
+
+	public void playerTurn(int x, int y, Player player) {
+		
+		// call private methods
+	
+	}
+
+}
